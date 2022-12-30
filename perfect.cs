9da4f50928a250cab625d05686f513816299d143
Randaelashry11 <121683386+Randaelashry11@users.noreply.github.com>
2022-12-30 18:07:54 +0200

    Add files via upload

---
+++ perfect.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pleas enter n1");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Pleas enter n2");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The perfect number is:");
+
+            for (int i = n1; i <= n2; i++)
+            {
+                int j = 1;
+                int sum = 0;
+                 while(j < i)
+                 {
+                    if (i % j == 0)
+                        sum = sum + j;
+                    j++;
+                 }
+                if (sum == i)
+               
+                    Console.WriteLine("{0}", i);
+            }
+                            Console.ReadKey();
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ prime project.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the first number");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the second number");
+            int second = int.Parse(Console.ReadLine());
+            for (int i = first; i <= second; i++)
+            {
+                int counter = 0;
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+                if (counter == 0 && i != 1)
+                {
+                    Console.WriteLine("The prime number is");
+                    Console.WriteLine("{0}", i);
+                }
+                    Console.ReadKey();
+            }
+
+
+
+        }
+    }
+}
